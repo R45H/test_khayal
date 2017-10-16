1a9db6a93a47e9588d83969573cfb8afbe377a99
R45H <gmr45h@gmail.com>
2017-10-16 23:01:37 +0300

    Add animation for toggle

---
+++ app/src/blocks/toggle/toggle.js
@@ -0,0 +1,18 @@
+var
+	$toggle = $('.toggle'),
+	activeClass = 'toggle_opened';
+
+/* Анимация гамбургера при клике */
+$toggle.on('click', function() {
+	$(this).toggleClass(activeClass);
+});
+/* ===== */
+
+/* Возврат гамбургера в исходное состояние при ресайзе */
+$(window).on('resize', function() {
+
+	if (window.innerWidth > 575) {
+		$toggle.removeClass(activeClass);
+	}
+});
+/* ===== */
--- app/src/script.js
+++ app/src/script.js
@@ -1,6 +1,9 @@
 $(function() {
-
 	@@include('global/global.js')
+
+	(function() {
+		@@include('blocks/toggle/toggle.js')
+	}());
 });
 
 @@include('global/functions.js')
